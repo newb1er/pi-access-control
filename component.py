9ffdfd881ee22a44bda46dd70e77abb0fa1f8034
newb1er <32424677+newb1er@users.noreply.github.com>
2021-11-30 19:12:21 +0000

    ⚰ remove component abstract class

--- component.py
+++
@@ -1,9 +0,0 @@
-from abc import ABCMeta, abstractmethod
-
-class Component(metaclass=ABCMeta):
-    @abstractmethod
-    def set_pin(self):
-        pass
-
-    def __init__(self):
-        self.set_pin()
